964bccaf0e41a88e7c578652030e3464f35ad377
naniitevez <nicole.tevez.97@gmail.com>
2023-04-25 23:49:07 -0300

    agrego react query
    creo hook y helpers para las peticiones y mapeo la data

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,33 @@
+import { LoadingIcon } from "../../shared/components/LoadingIcon";
+import { useLabels } from "../hooks/useLabels";
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker: React.FC<Props> = ({ selectedLabels, onChange }) => {
+  const labelsQuery = useLabels();
+
+  if (labelsQuery.isLoading) return <LoadingIcon />;
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      {labelsQuery.data?.map((label) => (
+        <span
+          key={label.id}
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(label.name) ? "label-active" : ""
+          }`}
+          style={{
+            border: `1px solid #${label.color}`,
+            color: `#${label.color}`,
+          }}
+          onClick={() => onChange(label.name)}
         >
-            Primary
+          {label.name}
         </span>
-        
+      ))}
     </div>
-  )
-}
+  );
+};
